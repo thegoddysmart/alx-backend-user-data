39bf76cb6fcc1773c57794a8b0945915784dea91
Godfred <goddysmart2468@gmail.com>
2024-09-30 08:44:30 +0000

    Database

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+""" Database for ORM """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """DB class to manage user operations."""
+
+    def __init__(self):
+        """ Initialize a new DB instance. """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ Session Getter Method """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a user to the database and return the User object.
+
+        Args:
+            email (str): The user's email.
+            hashed_password (str): The user's hashed password.
+
+        Returns:
+            User: The created User object.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user by given attributes.
+
+        Args:
+            **kwargs: The attributes to filter the user.
+
+        Returns:
+            User: The found User object.
+
+        Raises:
+            NoResultFound: If no user is found.
+            InvalidRequestError: If invalid query arguments are passed.
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        pdate user attributes by user ID.
+
+        Args:
+            user_id (int): The ID of the user to update.
+            **kwargs: The attributes to update.
+
+        Raises:
+            ValueError: If an invalid attribute is passed.
+        """
+        user = self.find_user_by(id=user_id)
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise ValueError
+
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
+        self._session.commit()
